aed0f48a99477c1322e0a83b78982fe0fe3f0481
frutodelaranja <frutodelaranja@gmail.com>
2021-09-12 16:03:48 +0300

    change the Employee class

--- src/main/java/com/evgenykozlov/construction_site/entity/Employee.java
+++ src/main/java/com/evgenykozlov/construction_site/entity/Employee.java
@@ -1,4 +1,39 @@
 package com.evgenykozlov.construction_site.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "salary")
+    private float salary;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REFRESH, CascadeType.REMOVE})
+    @JoinTable(name = "employees_skills", joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private Set<Skill> skills;
+
+
+
+
 }
+
